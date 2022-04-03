8b3edc7f8fd743b543141ab9f9dd57eb64f0d3b2
kozh-0 <kdaufa@mail.ru>
2022-04-04 01:03:41 +0300

    time updated

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ tabParent.addEventListener('click', (e) => {
 
 	/* TIMER ===========================================================*/
 
-const deadline = '2022-03-31';
+const deadline = '2022-04-31';
 	// Объект со временем
 function getTimeRemaining(endtime) { 
 	const t = Date.parse(endtime) - Date.parse(new Date()),
